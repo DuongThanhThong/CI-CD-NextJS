8923717981e8bb827e2e8a9c6165ea8cebeb7630
DuongThanhThong <thongduong61134@gmail.com>
2024-07-11 22:48:41 +0700

    fix production fail

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -8,6 +8,10 @@ export async function generateMetadata({ params }: Params) {
   return { title: `Post: ${params.slug}` };
 }
 
+export async function generateStaticParams() {
+  return []
+}
+
 export default function Page({ params }: Params) {
   return <h1>Slug: {params.slug}</h1>;
 }
