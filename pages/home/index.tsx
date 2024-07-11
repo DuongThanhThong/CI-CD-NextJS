2651118ef0eceb287525861bcd1b671e87a47f17
DuongThanhThong <thongduong61134@gmail.com>
2024-07-11 16:49:44 +0700

    test 123

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -22,7 +22,7 @@ export default function Home() {
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
             <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API. 123</p>
+            <p>Find in-depth information about Next.js features and API.</p>
           </a>
 
           <a href="https://nextjs.org/learn" className={styles.card}>
@@ -41,7 +41,7 @@ export default function Home() {
           <a href="https://vercel.com/new" className={styles.card}>
             <h3>Deploy &rarr;</h3>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              Instantly deploy your Next.js site to a public URL with Vercel. 1213
             </p>
           </a>
         </div>
